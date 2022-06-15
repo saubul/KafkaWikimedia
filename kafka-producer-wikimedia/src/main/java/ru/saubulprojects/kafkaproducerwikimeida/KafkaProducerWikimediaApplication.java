97bde00f660c2985c777308f56298da5e22b7d65
Ruslan <miraxle@yandex.ru>
2022-06-15 16:38:25 +0300

    Инициализация проекта

---
+++ kafka-producer-wikimedia/src/main/java/ru/saubulprojects/kafkaproducerwikimeida/KafkaProducerWikimediaApplication.java
@@ -0,0 +1,12 @@
+package ru.saubulprojects.kafkaproducerwikimeida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerWikimediaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerWikimediaApplication.class, args);
+	}
+
+}
